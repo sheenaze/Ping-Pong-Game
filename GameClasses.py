59afc59bd7fc532df4e50d90aa36cb261aec0e4c
Monika Tercjak <monika.tercjak@gmail.com>
2020-02-16 18:32:29 +0100

    The ball is flying

--- GameClasses.py
+++ GameClasses.py
@@ -1,50 +1,218 @@
 import pygame as pg
 import time
+
+
 # pg.init()
 
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-
-
-# size = (500, 700)
-# screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-# print(pygame.display.Info())
-# # screen = pygame.display.mode_ok(size)
-# pygame.display.set_caption("PingPong")
-#
-#
-#
-# carryOn = True
-#
-# # The clock will be used to control how fast the screen updates
-# clock = pygame.time.Clock()
-#
-# # -------- Main Program Loop -----------
-# while carryOn:
-#     # --- Main event loop
-#     for event in pygame.event.get():  # User did something
-#         if event.type == pygame.QUIT:  # If user clicked close
-#             carryOn = False  # Flag that we are done so we exit this loop
 
 class Board:
     def __init__(self, width, height):
-        self.window = pg.display.set_mode((width, height), pg.RESIZABLE)
+        self.width = width
+        self.height = height
+        self.window = pg.display.set_mode((width, height), 0, 32)
+        pg.display.set_caption("PingPong by Mon")
 
     def changeWindowColor(self, window_color):
+        self.window_color = window_color
         self.window.fill(window_color)
         pg.display.update()
 
-    # pg.display.set_caption('PingPong by Mon')
-
-    # def draw(self, *args):
-    #     background = (100, 100, 100)
-    #     self.window.fill(background)
-    #     for drawable in args:
-    #         drawable.draw_on(self.window)
-    #
-    #     pg.display.update()
+    def draw_elements(self, *args):
+        self.window.fill(self.window_color)
+        # pg.display.update()
+        for element in args:
+             element.draw(self.window)
+        pg.display.update()
 
 
-board = Board(500,700)
-board.changeWindowColor((100, 100, 200))
-time.sleep(10)
+
+
+class GameObject:
+    def __init__(self, width, height, x, y, color):
+        self.width = int(width)
+        self.height = int(height)
+        self.start_x = x
+        self.start_y = y
+        self.color = color
+        self.surface = pg.Surface((self.width, self.height), pg.SRCALPHA,  32).convert_alpha()
+        self.rect = self.surface.get_rect(x = self.start_x, y = self.start_y)
+        print(f'to jest Game Object self.rect: {self.rect}')
+
+    def draw(self, window):
+        window.blit(self.surface, self.rect)
+
+class Ball(GameObject):
+    def __init__(self, width, height, x, y, color, radius, x_speed = 0, y_speed = 0):
+        super(Ball, self).__init__(width, height, x, y, color)
+        circle = pg.draw.circle(self.surface, color, [radius, radius], radius)
+        print(f'to jest circle: {circle}')
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+
+    def bounce_x(self):
+        self.x_speed *= -1
+
+    def bounce_y(self):
+        self.y_speed *= -1
+
+    def move(self):
+        self.rect.x += self.x_speed
+        self.rect.y += self.y_speed
+        # pg.display.flip()
+        print(self.rect)
+
+
+# class GameObjects:
+#     def __init__(self, width, height, color, x, y):
+#         self.width = int(width)
+#         self.height = int(height)
+#         self.color = color
+#         self.surface = pg.Surface((self.width, self.height),  pg.SRCALPHA, 32).convert_alpha()
+#         self.rect = self.surface.get_rect(x = x, y = y)
+#         print(f'to jest Game Object self.rect: {self.rect}')
+#
+#     def draw_on(self, window):
+#         window.blit(self.surface, self.rect)
+#
+# class Ball(GameObjects):
+#     def __init__(self, width, height,  color, radius,  x_vector = 0, y_vector=0):
+#         super(Ball, self).__init__(width, height, color, x, y)
+#         self.radius = int((width**2+height**2)**(1/2)*0.5)
+#         circle = pg.draw.circle(self.surface, self.color, [0, 0], self.radius)
+#         print(f'to jest circle: {circle}')
+#         #
+#         # self.surface = pg.Surface([width, heigth])
+#         # self.ball = pg.draw.circle(self.surface, color, location, radius)
+#         # self.rect = self.surface.get_rect()
+#         # print(self.rect)
+
+    # def draw_ball(self, surface):
+    #     surface.blit(self.surface, self.rect)
+
+
+width = 1000
+height = 1000
+window_color = (100, 100, 200)
+object_color = (255,255,255)
+x = int(width/2)
+y = int(height/2)
+radius = 10
+# board = Board(width,height)
+# print(type(board))
+# board.changeWindowColor(window_color)
+
+# object = GameObjects(int(width*0.02), int(height*0.02), object_color, x, y)
+
+# surface = pg.display.set_mode((width, height))
+# surface.fill(window_color)
+# pg.display.update()
+
+# ball = Ball(int(radius*2), int(2*radius) ,x, y, object_color,  radius)
+# print(ball.rect)
+# board.draw_elements(window_color, ball)
+# circle = pg.draw.circle(surface, object_color, [x, y], 10)
+# pg.display.flip()
+
+# width, height, x, y, color, radius):
+
+# print(circle)
+
+
+# time.sleep(10)
+
+
+
+
+
+
+
+# class Board:
+#     def __init__(self, width, height):
+#         self.surface = pg.display.set_mode((width, height))
+#         pg.display.set_caption('PingPong by Mon')
+#
+#     def window_look(self, window_color = (0,0,0)):
+#         self.surface.fill(window_color)
+#         pg.display.update()
+#
+#     def draw_object(self, *args):
+#         """
+#         Rysuje okno gry
+#
+#         :param args: lista obiektów do narysowania
+#         """
+#         background = (230, 255, 255)
+#         self.surface.fill(background)
+#         for drawable in args:
+#             drawable.draw_on(self.surface)
+#
+#         # dopiero w tym miejscu następuje fatyczne rysowanie
+#         # w oknie gry, wcześniej tylko ustalaliśmy co i jak ma zostać narysowane
+#         pg.display.update()
+#
+#     # def draw_object(self, *args):
+#     #     for element in args:
+#     #         element.draw_on(self.surface)
+#     #     pg.display.flip()
+#     # # def draw_object(self, *args):
+#     #     for element in args:
+#     #         element.draw_element(self.surface)
+#     #     pg.display.update()
+#
+# class Drawable(object):
+#     """
+#     Klasa bazowa dla rysowanych obiektów
+#     """
+#
+#     def __init__(self, width, height, x, y, color=(0, 255, 0)):
+#         self.width = width
+#         self.height = height
+#         self.color = color
+#         self.surface = pg.Surface([width, height], pg.SRCALPHA, 32).convert_alpha()
+#         self.rect = self.surface.get_rect(x=x, y=y)
+#
+#     def draw_on(self, surface):
+#         surface.blit(self.surface, self.rect)
+#
+# class Ball(Drawable):
+#     """
+#     Piłeczka, sama kontroluje swoją prędkość i kierunek poruszania się.
+#     """
+#     def __init__(self, width, height, x, y, color=(255, 0, 0), x_speed=3, y_speed=3):
+#         super(Ball, self).__init__(width, height, x, y, color)
+#         # pg.draw.ellipse(self.surface, self.color, [0, 0, self.width, self.height])
+#         pg.draw.circle(self.surface, self.color,[0,0], 10, 10)
+#
+# # class Ball:
+# #     def __init__(self, width, height, color, position, radius):
+# #         self.x = position[0]
+# #         self.y = position[1]
+# #         self.surface = pg.Surface((width, height), pg.SRCALPHA).convert_alpha()
+# #         self.rect = self.surface.get_rect(x = self.x, y = self.y)
+# #         pg.draw.circle(self.surface, color, position, radius)
+# #         # pg.display.update()
+# #
+# #     def draw_on(self, surface):
+# #         surface.blit(self.surface, self.rect)
+#
+#
+# width = 1000
+# height = 1000
+# x = int(width/2)+100
+# y = int(height/2)
+# object_color = (255, 255, 255)
+# window_color = (10, 200, 100)
+# radius = 10
+#
+# board = Board(width, height)
+# board.window_look(window_color)
+# # ball = Ball((width*0.02),(height*0.02), object_color, [x, y], radius)
+# ball = Ball(20, 20, int(width/2), int(height/2), object_color)
+#
+# board.draw_object(ball)
+# # pg.draw.circle(board.surface, object_color, [x, y], radius)
+# # pg.display.update()
+# # print(board.surface)
+# time.sleep(10)
+#
+
--- PingPong.py
+++ PingPong.py
@@ -1,15 +1,57 @@
 import pygame as pg
 from GameClasses import *
+import pygame.locals
+
+window_color = (10,200,100)
+width = 1000
+height = 1000
+x = int(width/2)
+y = int(height/2)
+object_color = (255, 255, 255)
+radius = 10
 
 class PingPong:
-    def __init__(self, width, height, window_color):
+    def __init__(self, window_width = width, window_height = height, window_color = window_color, paddles_color = object_color, ball_radius = radius, ball_color = object_color):
         pg.init()
-        self.window = Board(width, height)
+
+        self.window = Board(window_width, window_height)
+        self.window.changeWindowColor(window_color)
+        self.x = int(width / 2)
+        self.y = int(height / 2)
+        self.fps_clock = pygame.time.Clock()
+        self.ball = Ball(ball_radius*2, ball_radius*2, self.x, self.y, object_color, ball_radius, x_speed=1, y_speed=3)
+
+
+    def run(self):
+        i = 0
+        timeout = 10  # [seconds]
+        timeout_start = time.time()
+        print(timeout_start)
+        # while time.time() < timeout_start + timeout:
+        while not self.handle_events():
+            if self.ball.rect.y >= self.window.height or self.ball.rect.y <= 0:
+                self.ball.bounce_y()
+
+            if self.ball.rect.x >= self.window.width  or self.ball.rect.x <= 0:
+                self.ball.bounce_x()
+
+            self.ball.move()
+            self.window.draw_elements(self.ball)
+            self.fps_clock.tick(40)
+
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pygame.locals.QUIT:
+                pygame.quit()
+                return True
 
 
 
 
 if __name__ == "__main__":
-    game = PingPong(800, 400, (10, 100, 200))
+    game = PingPong()
+    game.run()
+    # game.windowLook((10, 100, 200))
     time.sleep(10)
+    pg.quit()
     # game.run()
