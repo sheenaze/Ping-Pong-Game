e592fc2de07c60dc92710d166e0946c98b777766
Monika Tercjak <monika.tercjak@gmail.com>
2020-03-02 21:16:13 +0100

    main file changed according to new classes

--- PingPong.py
+++ PingPong.py
@@ -1,28 +1,25 @@
-
 from GameClasses import *
 import pygame.locals
 from pynput import keyboard
-
-
-
 
 class PingPong:
     def __init__(self, window_width, window_height, window_color, paddles_color, ball_radius, ball_color):
         pg.init()
 
         self.window = Board(window_width, window_height, window_color)
-        self.game_objects = GameObjects(window_width, window_height, paddles_color, ball_radius, ball_color)
+        self.game_objects = CurrentGameObjects(window_width, window_height, paddles_color, ball_radius, ball_color)
 
         self.ball = self.game_objects.get_ball()
         self.left_paddle = self.game_objects.get_left_paddle()
         self.right_paddle = self.game_objects.get_right_paddle()
 
+        self.actions = GameActions(self.window, self.ball, self.left_paddle, self.right_paddle)
+
         self.fps_clock = pygame.time.Clock()
     def run(self):
 
         while not self.handle_events():
-            self.ball.move()
-
+            self.actions.ball_movement()
             self.window.draw_elements(
                 self.ball,
                 self.left_paddle,
